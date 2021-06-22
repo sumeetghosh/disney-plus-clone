a6f7a95f253058ef9231b6f8f545d06529e3b1dd
Sumeet Ghosh <sumeetghosh1990@gmail.com>
2021-06-22 23:57:16 +0530

    ADD: Viewes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlider from "../components/ImgSlider";
+import Viewers from "../components/Viewers";
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
     </Container>
   );
 }
