3bcc5253778855183a1bc1fa9a41f87221b3a9ae
Sumeet Ghosh <sumeetghosh1990@gmail.com>
2021-06-23 11:30:11 +0530

    ADD: Detail and Login Page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlider from "../components/ImgSlider";
+import Movies from "../components/Movies";
 import Viewers from "../components/Viewers";
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
     <Container>
       <ImgSlider />
       <Viewers />
+      <Movies />
     </Container>
   );
 }
